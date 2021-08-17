9f3b095937de9dac144b8bdf07bad12a323af117
Cihan Emre Kisakurek <mail@cihan.rocks>
2021-08-17 20:18:04 +0200

    pods removed

--- Pods/Target Support Files/Pods-DataAnalyzer/Pods-DataAnalyzer-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DataAnalyzer : NSObject
-@end
-@implementation PodsDummy_Pods_DataAnalyzer
-@end
--- Pods/Target Support Files/Pods-DataAnalyzerTests/Pods-DataAnalyzerTests-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_DataAnalyzerTests : NSObject
-@end
-@implementation PodsDummy_Pods_DataAnalyzerTests
-@end
